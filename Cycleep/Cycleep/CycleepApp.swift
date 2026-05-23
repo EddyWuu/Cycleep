8a9a52af3d8e2bcd1cd237c1d01128637007ce0a
Edmond Wu <edmond.wuca@gmail.com>
2026-05-22 22:12:12 -0700

    Cycleep init

---
+++ Cycleep/Cycleep/CycleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleepApp.swift
+//  Cycleep
+//
+//  Created by Edmond Wu on 2026-05-22.
+//
+
+import SwiftUI
+
+@main
+struct CycleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cycleep/CycleepTests/CycleepTests.swift
@@ -0,0 +1,17 @@
+//
+//  CycleepTests.swift
+//  CycleepTests
+//
+//  Created by Edmond Wu on 2026-05-22.
+//
+
+import Testing
+@testable import Cycleep
+
+struct CycleepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
